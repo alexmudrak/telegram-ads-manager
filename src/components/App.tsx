82754f53c49869a2561e4fc3e86d180bf75b58ef
alexmudrak <95851199+alexmudrak@users.noreply.github.com>
2025-04-29 18:42:28 +0300

    build: add prettier

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import { Button } from 'antd';
 
 function App() {
-    return (
-        <>
-            <Button className='text-center w-full' type='primary'>Button</Button>
-        </>
-    );
+  return (
+    <>
+      <Button className='text-center w-full' type='primary'>
+        Button
+      </Button>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './components/App.tsx'
+import App from './components/App.tsx';
+import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
