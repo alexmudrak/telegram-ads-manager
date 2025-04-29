30fe8f8e7c4f258b862d4fdb662919f94afda497
alexmudrak <95851199+alexmudrak@users.noreply.github.com>
2025-04-29 18:38:40 +0300

    feat: add base APP

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'antd';
+
+function App() {
+    return (
+        <>
+            <Button className='text-center w-full' type='primary'>Button</Button>
+        </>
+    );
+}
+
+export default App;
