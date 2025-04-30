4b87b091009e165233cd0c4d746e1de615070e8b
alexmudrak <95851199+alexmudrak@users.noreply.github.com>
2025-04-30 15:34:33 +0300

    feat: save to DB only valid channel data

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -11,7 +11,7 @@ use serde_json::json;
 use std::collections::HashMap;
 use std::env;
 use std::fs;
-use std::path::Path;
+use std::sync::{Arc, Mutex};
 
 #[derive(Deserialize)]
 struct ChannelRequest {
@@ -31,7 +31,7 @@ struct Channel {
 }
 
 #[derive(Deserialize, Serialize)]
-struct ApiResponse {
+struct ApiSimilarChannelsResponse {
     ok: bool,
     channels: Vec<Channel>,
 }
@@ -39,14 +39,15 @@ struct ApiResponse {
 #[derive(Serialize)]
 struct ChannelResponse {
     id: i64,
-    name: String,
-    link: String,
+    title: String,
+    username: String,
     photo_element: String,
+    category: Option<String>,
+    description: String,
     subscribers: String,
-    category: String,
 }
 
-impl Default for ApiResponse {
+impl Default for ApiSimilarChannelsResponse {
     fn default() -> Self {
         Self {
             ok: false,
@@ -55,11 +56,13 @@ impl Default for ApiResponse {
     }
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Clone)]
 struct Chat {
     id: i64,
+    title: String,
     username: String,
-    description: String,
+    description: Option<String>,
+    category: Option<String>,
 }
 
 #[derive(Deserialize)]
@@ -106,15 +109,25 @@ fn extract_subscribers(cb_item: &str) -> String {
     "0".to_string()
 }
 
-async fn get_category(client: &Client, cb_item: &str) -> Result<String, String> {
+async fn fetch_chat_category(client: &Client, data: &str) -> Result<String, String> {
     let endpoint = "https://api.openai.com/v1/chat/completions";
     let api_key = env::var("APP_OPENAI_API_KEY").expect("APP_OPENAI_API_KEY not set");
+
+    let available_categories = vec![
+        "новости",
+        "путешествия",
+        "развлекательные",
+        "политика",
+        "обучение",
+        "работа",
+    ];
+    let categories_str = available_categories.join(", ");
 
     let request_body = json!({
         "model": "gpt-4o-mini",
         "messages": [{
             "role": "user",
-            "content": format!("На основании данных {}, выдели категорию из возможных. Возвращай только категорию из существующих новости, путешествия, развлекательные, политика, обучение, работа. Не пиши никакого дополнительного текста", cb_item),
+            "content": format!("На основании данных {}, выдели категорию из возможных. Возвращай только категорию из существующих {}. Не пиши никакого дополнительного текста", data, categories_str),
         }],
         "max_tokens": 50,
     });
@@ -129,17 +142,21 @@ async fn get_category(client: &Client, cb_item: &str) -> Result<String, String> 
         .map_err(|e| e.to_string())?;
 
     if response.status().is_success() {
-        info!("Received successful response from OpenAI API.");
-
-        let response_json: serde_json::Value = response.json().await.map_err(|e| {
-            error!("Failed to parse response JSON: {}", e);
-            e.to_string()
-        })?;
+        let response_json: serde_json::Value = response.json().await.map_err(|e| e.to_string())?;
 
         if let Some(choice) = response_json["choices"].as_array().and_then(|c| c.get(0)) {
             if let Some(content) = choice["message"]["content"].as_str() {
-                info!("Extracted category: {}", content);
-                return Ok(content.to_lowercase().to_string());
+                let category = content.to_lowercase();
+                if available_categories.iter().any(|&cat| cat == category) {
+                    return Ok(category);
+                } else {
+                    let err_msg = format!(
+                        "Received category '{}' is not in available categories.",
+                        category
+                    );
+                    error!("{}", err_msg);
+                    return Err(err_msg);
+                }
             }
         }
         let err_msg = "Failed to extract category from response.".to_string();
@@ -179,10 +196,16 @@ async fn fetch_channel_info(
                     .strip_prefix("-100")
                     .unwrap_or(&chat.id.to_string())
                     .to_string();
+
+                info!("Done Fetching info for channel: {}", channel_name);
+                let description = chat
+                    .description
+                    .unwrap_or_else(|| "No description".to_string());
+
                 return Ok((
                     clean_id.parse().unwrap_or_default(),
                     chat.username,
-                    chat.description,
+                    description,
                 ));
             } else {
                 return Err("Channel not found".to_string());
@@ -209,18 +232,17 @@ async fn get_channels_id(client: &Client, channels: &str) -> Result<String, Stri
             .replace("/", "")
             .replace(" ", "");
 
-        info!("Processing channel: {:?}", channel_name);
-
         if let Some(existing_channel) = db.channels.iter().find(|c| c.username == channel_name) {
             channels_with_ids.push(existing_channel.id.to_string());
-            info!("Found existing channel: {}", existing_channel.username);
         } else {
             match fetch_channel_info(client, &channel_name).await {
                 Ok((channel_id, username, description)) => {
                     let new_channel = Chat {
                         id: channel_id,
+                        title: "".to_string(),
                         username: username.to_string(),
-                        description: description.to_string(),
+                        description: Some(description),
+                        category: None,
                     };
                     db.channels.push(new_channel);
                     channels_with_ids.push(channel_id.to_string());
@@ -239,31 +261,22 @@ async fn get_channels_id(client: &Client, channels: &str) -> Result<String, Stri
     Ok(channels_with_ids.join(";"))
 }
 
-async fn get_similar_channels(
-    client: web::Data<Client>,
-    req: web::Json<ChannelRequest>,
-) -> HttpResponse {
+async fn fetch_similar_channels(
+    client: &Client,
+    channels_ids: String,
+    req: &ChannelRequest,
+) -> Result<Vec<ChannelResponse>, String> {
+    let file_path = "channels.json";
+    let db =
+        load_channels_from_file(file_path).unwrap_or_else(|_| ChannelDatabase { channels: vec![] });
+    let db = Arc::new(Mutex::new(db));
+
     let url = format!("https://ads.telegram.org/api?hash={}", req.hash);
     let mut form = HashMap::new();
 
-    match get_channels_id(&client, &req.channels).await {
-        Ok(channels_ids) => {
-            info!("TEST 2: {:?}", channels_ids);
-            form.insert("channels".to_string(), channels_ids);
-        }
-        Err(err) => {
-            eprintln!("Error while getting channels ID: {}", err);
-            return HttpResponse::InternalServerError().body("Failed to get channel IDs");
-        }
-    }
+    form.insert("channels".to_string(), channels_ids);
     form.insert("for".to_string(), "channels".to_string());
     form.insert("method".to_string(), "getSimilarChannels".to_string());
-
-    info!(
-        "Sending request to Telegram API for channels: {}",
-        req.channels
-    );
-
     let response = client
         .post(&url)
         .header("Accept", "application/json, text/javascript, */*; q=0.01")
@@ -278,55 +291,143 @@ async fn get_similar_channels(
         )
         .form(&form)
         .send()
-        .await;
+        .await
+        .map_err(|e| e.to_string())?;
 
-    match response {
-        Ok(resp) => {
-            let status = resp.status();
-            let body = resp
-                .text()
-                .await
-                .unwrap_or_else(|_| "Failed to read response".to_string());
-            if status.is_success() {
-                info!("Successfully received response from Telegram API");
-                let api_response: ApiResponse = serde_json::from_str(&body).unwrap_or_default();
-                let channels = api_response.channels;
+    if response.status().is_success() {
+        let body = response.text().await.map_err(|e| e.to_string())?;
+        let api_response: ApiSimilarChannelsResponse =
+            serde_json::from_str(&body).map_err(|e| e.to_string())?;
+        let channels = api_response.channels;
 
-                let tasks: Vec<_> = channels
-                    .into_iter()
-                    .map(|channel| {
-                        let client_clone = client.clone();
-                        let cb_item_clone = channel.cb_item.clone();
-                        let subscribers_data = extract_subscribers(&channel.cb_item);
+        let mut channel_responses: Vec<ChannelResponse> = vec![];
+        let mut fetch_tasks = vec![];
 
-                        async move {
-                            let category = get_category(&client_clone, &cb_item_clone)
-                                .await
-                                .unwrap_or("NONE".to_string());
+        for channel in channels {
+            let db_clone = Arc::clone(&db);
+            let subscribers_count = extract_subscribers(&channel.cb_item);
+            let existing_channel;
 
-                            ChannelResponse {
-                                id: channel.id,
-                                name: channel.title.clone(),
-                                link: format!("https://t.me/{}", channel.username),
-                                photo_element: channel.photo,
-                                subscribers: subscribers_data,
-                                category,
-                            }
-                        }
-                    })
-                    .collect();
+            {
+                let mut db_guard = db_clone.lock().unwrap();
+                existing_channel = db_guard
+                    .channels
+                    .iter()
+                    .find(|c| c.id == channel.id)
+                    .cloned();
 
-                let channel_responses: Vec<ChannelResponse> = join_all(tasks).await;
+                if let Some(existing) = &existing_channel {
+                    if existing.title != channel.title
+                        || existing.category.is_none()
+                        || existing.description.is_none()
+                    {
+                        db_guard.channels.retain(|c| c.id != existing.id);
+                    }
+                }
+            }
 
-                HttpResponse::Ok().json(channel_responses)
+            if let Some(existing) = existing_channel {
+                let description = existing
+                    .description
+                    .clone()
+                    .unwrap_or_else(|| "No description".to_string());
+
+                channel_responses.push(ChannelResponse {
+                    id: channel.id,
+                    title: channel.title.clone(),
+                    username: channel.username.clone(),
+                    photo_element: channel.photo,
+                    category: existing.category.clone(),
+                    description,
+                    subscribers: subscribers_count,
+                });
             } else {
-                error!("Error from Telegram API: {}", body);
-                HttpResponse::InternalServerError().body(format!("Error: {}", body))
+                let client_clone = client.clone();
+                let channel_username_clone = channel.username.clone();
+
+                let task = async move {
+                    let (channel_id, username, description) =
+                        fetch_channel_info(&client_clone, &channel_username_clone)
+                            .await
+                            .unwrap_or((
+                                0,
+                                "No username".to_string(),
+                                "No description".to_string(),
+                            ));
+
+                    let mut category = "".to_string();
+
+                    if channel_id != 0 {
+                        let combined_description = format!("{} {}", channel.title, description,);
+
+                        category = fetch_chat_category(&client_clone, &combined_description)
+                            .await
+                            .unwrap_or("UNKNOWN".to_string());
+                        let mut db_guard = db_clone.lock().unwrap();
+                        db_guard.channels.push(Chat {
+                            id: channel_id,
+                            title: channel.title.to_string(),
+                            username: username.to_string(),
+                            category: Some(category.clone()),
+                            description: Some(description.clone()),
+                        });
+                    }
+
+                    ChannelResponse {
+                        id: channel_id,
+                        title: channel.title.clone(),
+                        username,
+                        photo_element: channel.photo,
+                        category: Some(category),
+                        description,
+                        subscribers: subscribers_count,
+                    }
+                };
+
+                fetch_tasks.push(task);
             }
         }
-        Err(e) => {
-            error!("Request error: {}", e);
-            HttpResponse::InternalServerError().body(format!("Request error: {}", e))
+
+        let additional_responses: Vec<ChannelResponse> = join_all(fetch_tasks).await;
+
+        channel_responses.extend(additional_responses);
+
+        save_channels_to_file(file_path, &db.lock().unwrap()).map_err(|e| e.to_string())?;
+
+        return Ok(channel_responses);
+    } else {
+        return Err(format!("Error from Telegram API: {}", response.status()));
+    }
+}
+
+async fn get_similar_channels(
+    client: web::Data<Client>,
+    req: web::Json<ChannelRequest>,
+) -> HttpResponse {
+    match get_channels_id(&client, &req.channels).await {
+        Ok(channels_ids) => {
+            info!(
+                "Sending request to Telegram API for channels: {}",
+                channels_ids
+            );
+
+            let similars_channels = fetch_similar_channels(&client, channels_ids, &req).await;
+
+            match similars_channels {
+                Ok(channel_responses) => {
+                    info!("All Done");
+                    return HttpResponse::Ok().json(channel_responses);
+                }
+                Err(err) => {
+                    eprintln!("Error while fetching similar channels: {}", err);
+                    return HttpResponse::InternalServerError()
+                        .body("Failed to fetch similar channels");
+                }
+            }
+        }
+        Err(err) => {
+            eprintln!("Error while getting channels ID: {}", err);
+            return HttpResponse::InternalServerError().body("Failed to get channel IDs");
         }
     }
 }
